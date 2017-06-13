0cf6820cee7b71292168c5aecf469c0fcd6cae22
garcial <garcial@uji.es>
2017-06-13 17:01:25 +0200

    ToJSon static

--- RoadSImV2V/src/org/json/ToJSON.java
+++ RoadSImV2V/src/org/json/ToJSON.java
@@ -1,12 +1,9 @@
 package org.json;
 
-public class TOJSON {
+public class ToJSON {
 
-	public TOJSON() {
-		// TODO Auto-generated constructor stub
-	}
 	
-	private String toJSon(Object...objects) {
+	public static String toJSon(Object...objects) {
         StringBuilder cadena = new StringBuilder("{");
         for (int i = 0; i < objects.length; i+=2) {
             cadena.append(objects[i].toString()).
@@ -17,7 +14,7 @@ public class TOJSON {
         return cadena.append("}").toString();
     }
     
-    private String toJSonList(Object...objects) {
+    public static String toJSonList(Object...objects) {
         StringBuilder build = new StringBuilder("[");
         for(int i = 0; i< objects.length; i++) 
             build.append(objects[i]).append(",");
